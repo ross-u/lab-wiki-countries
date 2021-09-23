1974f81aac8a942ca6dbf1aa3c74b7e9e60fd6a5
Osvaldo Picazo <odpicazo@gmail.com>
2021-09-23 18:28:15 +0200

    [WDFT - Aug21 - RMT] Osvaldo Picazo - Labo done until it. 3

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import countries from "./countries.json"
+import {useState, useEffect} from 'react';
+import CountriesList from './components/CountriesList';
+import { Switch, Route, Redirect } from "react-router-dom";
+import CountryDetails from './components/CountryDetails';
+
+
+function App() {
+
+  //useState for the countries data
+  const [ countriesData, setCountriesData] = useState([])
+
+  useEffect(() => {
+    setCountriesData(countries)
+    console.log("mounting")
+    return () => {
+  
+    }
+  }, [])
+  
+  return (
+    <div className="App">
+      <Navbar/>
+        <Switch>
+          <Route 
+          path='/:countryAlpha3Code'
+          render={(routeProps) => <CountryDetails {...routeProps} countriesData={countriesData} /> } 
+          />
+          <Route
+          exact path="/"
+          render={(routeProps) => <CountriesList {...routeProps} countriesData={countriesData} /> } 
+          />
+          {/* <CountriesList countriesData={countriesData} /> */}
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
