5b5cfd9284afd89b17b817a3cfe86adada2a7343
lauraAlabau <arlaura3@gmail.com>
2021-09-23 19:29:09 +0200

    Iteration 3 | Set the state when the component mounts

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import CountriesList from "./components/CountriesList";
 import CountryDetails from "./components/CountryDetails";
 import { Switch, Route } from "react-router-dom";
 import coutriesData from './countries.json'
+import { useState, useEffect } from "react";
 
-function App() {
+function App() {  
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    setCountries(coutriesData);
+    
+}, []);
+
   return (
     <div className="App">
      <Navbar />
       <div className="container">
         <div className="row">
           <div className="col-6">
-            <CountriesList countries={coutriesData}/>
+            <CountriesList countries={countries}/>
           </div>
           <div className="col-6">
             <Switch>
                 <Route 
                   path="/countries/:alpha3Code" 
                   render={(routeProps) => {
-                    return <CountryDetails {...routeProps} countries={coutriesData} />;
+                    return <CountryDetails {...routeProps} countries={countries} />;
                   }}
                 />
             </Switch>
