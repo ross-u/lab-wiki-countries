cdda297ecf2a9a5e3027381b01151b578cc9d916
Rahaf <r.abualhassan@gmail.com>
2021-09-23 19:50:19 +0200

    iterations 1 to 1.4 done. Here I added a home page to play around with Bootstrap carousel. This needs to be removed for the next commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './componants/Home';
+import Navbar from './componants/Navbar';
+import CountriesList from './componants/CountriesList';
+import { useState, useEffect } from 'react';
+
+
+import { Route, Switch } from 'react-router-dom';
+import CountryDetails from './componants/CountryDetails';
+
+
+function NotFound() {
+  return <h1>404 - Not Found - 🙀</h1>
+}
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    setCountries(CountriesList)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route  path="/countries" component={CountriesList} />
+        <Route exact path="/:alpha3Code" render={(props) => <CountryDetails {...props} countries={countries}/> } />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ src/componants/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+            <Link className="navbar-brand mb-0 h1" to="/">WikiCountries</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/countries">Countries</Link>
+                        </li>
+                        
+                        <li className="nav-item"></li>
+                    </ul>
+            </div>    
+                </div>
+            
+        </nav>
+    )
+}
