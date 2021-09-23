00a1c38f73438b997c04fd3cbd4bb2071034e7c0
Rahaf <r.abualhassan@gmail.com>
2021-09-23 20:32:14 +0200

    iteration 2 and 3 done, more styling still to be done

--- src/App.js
+++ src/App.js
@@ -4,33 +4,36 @@ import Home from './componants/Home';
 import Navbar from './componants/Navbar';
 import CountriesList from './componants/CountriesList';
 import { useState, useEffect } from 'react';
-
+import countriesArr from './countries.json'
 
 import { Route, Switch } from 'react-router-dom';
 import CountryDetails from './componants/CountryDetails';
-
-
-function NotFound() {
-  return <h1>404 - Not Found - 🙀</h1>
-}
 
 function App() {
 
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-    setCountries(CountriesList)
+    setCountries(countriesArr)
   }, [])
 
   return (
     <div className="App">
-      <Navbar />
+    <Navbar />
+    <div className="container">
+      <div className="row">
+        <div className="col-5" style={{maxHeight: "90vh", overflow: "scroll"}}>
+          <CountriesList countries={countries}/>
+        </div>
+        <div className="col-7">
+          <Route exact path="/:alpha3Code" render={(props) => <CountryDetails {...props} countries={countries}/> } />
+      </div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route  path="/countries" component={CountriesList} />
-        <Route exact path="/:alpha3Code" render={(props) => <CountryDetails {...props} countries={countries}/> } />
-        <Route component={NotFound} />
-      </Switch>
+        
+        </Switch>
+      </div>
+    </div>
     </div>
   );
 }
--- src/componants/CountriesList.js
+++ src/componants/CountriesList.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import countriesArr from '../countries.json';
+
+
 
 export default function CountriesList(props) {
-    const countriesList = countriesArr.map(country => {
+    const countriesList = props.countries.map(country => {
         return (
             <div class="container" key={country.alpha3Code}>
-                <div className="row">
-                <div className="col-5">
-                <div className="list-group">
-                <Link className="list-group-item list-group-item-action" to={`/${country.alpha3Code}`}>
-                <img src={`https://www.countryflags.io/${country.alpha2Code}/shiny/64.png`} />
+                 <div className="row">
+                 <div className="col-5">
+                 <div className="list-group">
+                 <Link className="list-group-item list-group-item-action" to={`/${country.alpha3Code}`}>
+                 <img src={`https://www.countryflags.io/${country.alpha2Code}/shiny/64.png`} />
                         {country.name}
-                    </Link>
-                </div>
-                </div>
-                </div>
-            </div>
+                     </Link>
+                 </div>
+                 </div>
+                 </div>
+             </div>
         )
-    });
-    
+    })
     return (
-        <div>
+        <div >
             {countriesList}
         </div>
     )
 }
+
+// export default function CountriesList(props) {
+//     const countriesList = countriesArr.map(country => {
+//         return (
+//             <div class="container" key={country.alpha3Code}>
+//                 <div className="row">
+//                 <div className="col-5">
+//                 <div className="list-group">
+//                 <Link className="list-group-item list-group-item-action" to={`/${country.alpha3Code}`}>
+//                 <img src={`https://www.countryflags.io/${country.alpha2Code}/shiny/64.png`} />
+//                         {country.name}
+//                     </Link>
+//                 </div>
+//                 </div>
+//                 </div>
+//             </div>
+//         )
+//     });
+    
+//     return (
+//         <div>
+//             {countriesList}
+//         </div>
+//     )
+// }
