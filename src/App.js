b692212d1b002a8bec606f5c9f4683ecc7a152ba
JesusManuelMallo <88166930+JesusManuelMallo@users.noreply.github.com>
2021-09-23 21:58:20 +0200

    WDFT082021 (BCN) -Jesus Mallo
    Still working on it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import CountriesList from './components/CountriesList';
-import CountryDetails from './components/CountryDetails';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-      <Route render={(props) => <SomeComponent {...props} otherProp={someData} > } />
-        
-     
-    </div>
-  );
+  return <div className="App"></div>;
 }
-
 export default App;
