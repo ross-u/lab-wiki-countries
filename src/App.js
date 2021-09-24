ea7f6cfbd6c9f682ffef13a3ef4d6e7349f2f4b2
Adrian Fernandez Carmona <adrianfdezbcn@gmail.com>
2021-09-24 14:02:04 +0200

    DONE
    Me faltaba la it3, porque no entendia bien el funcionamiento de ese useEffect que piden.
    
    Ya esta hecho faltan los BONUS

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import { Route, Switch } from 'react-router';
 import './App.css';
 import Navbar from './components/Navbar';
-import countriesData from './countries.json'
-import CountriesList from './components/CountriesList'
-import CountryDetails from './components/CountryDetails'
-
-
+import countriesData from './countries.json';
+import CountriesList from './components/CountriesList';
+import CountryDetails from './components/CountryDetails';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState();
+
+  useEffect(() => {
+    setCountries(countriesData);
+  }, []);
+
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="container">
         <div className="row">
+          <CountriesList countries={countriesData} />
 
-        <CountriesList  countries={countriesData} />
-            
-      <Switch>
-
-        <Route
-            exact
-            path="/:countryAlpha3Code"
-            render={(routeProps) => {
-              return <CountryDetails {...routeProps} countries={countriesData} />;
-            }}
+          <Switch>
+            <Route
+              exact
+              path="/:countryAlpha3Code"
+              render={(routeProps) => {
+                return (
+                  <CountryDetails {...routeProps} countries={countriesData} />
+                );
+              }}
             />
-
-      </Switch>
-      </div>
+          </Switch>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";     // <== IMPORT
 
 function Navbar() {
   return (
