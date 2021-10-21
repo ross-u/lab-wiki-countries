847311d3e6ddf167d9718c57846bb707cea20e76
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2021-10-21 22:30:10 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ function Navbar() {
 		<nav className="navbar navbar-dark bg-primary mb-3">
 			<div className="container">
 				<a className="navbar-brand" href="/">
-					<u>WikiCountries</u>
+					<b>WikiCountries</b>
 				</a>
 			</div>
 		</nav>
