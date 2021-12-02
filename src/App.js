056e3be25feec3059d8f91163db7cf394b7f9d00
abdelmalek.saksioui <abdelmalek.saksioui@cgm.com>
2021-12-02 16:42:02 +0100

    bugged

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import CountriesDetails from "./components/CountriesDetails";
+import CountriesList from "./components/CountriesList";
+import countries from "./countries.json"
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Navbar/>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-5" style={{'max-height': '90vh', 'overflow': 'scroll'}}>
+                        <div className="list-group">
+                            {
+                                countries.map(country => <CountriesList country={country}/>)
+                            }
+                        </div>
+                    </div>
+                    <div className="col-7">
+                        {/*<CountriesDetails/>*/}
+                        {/*<Routes>*/}
+                        {/*    <Route path="/:alpha3code" element={<CountriesDetails/>}/>*/}
+                        {/*</Routes>*/}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+
+        <nav className="navbar navbar-dark bg-primary mb-3">
+            <div className="container">
+                <Link class="navbar-brand" href="/">WikiCountries</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
