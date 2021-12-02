6fedce32aeaa028165965b1eadf7da9e70dd70e8
Santi Jimenez <jimenez.santi@gmail.com>
2021-12-02 21:06:18 +0100

    Iteration 1.4 & 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import CountriesList from './components/CountriesList/CountriesList';
+import CountryDetails from './components/CountryDetails/CountryDetails';
 import Navbar from './components/Navbar/Navbar';
-
 import countriesFromJSON from './countries.json'
 
 function App() {
-  
+ 
   const [countries, setCountries] = useState(countriesFromJSON)
 
   return (
     <div className="App">
       <Navbar />
-      <CountriesList countries={countries}/>
+      <div className="container">
+        <div className="row">
+          <CountriesList countries={countries}/>
+          <Switch>
+            <Route exact path="/:id" render={() => <CountryDetails countries={countries}/>} />
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/CountriesList/CountriesList.js
+++ src/components/CountriesList/CountriesList.js
@@ -4,19 +4,19 @@ const CountriesList = (props) => {
   const { countries } = props;
 
   return (
-    <div className="col-5">
+    <div className="col-5" style={{"max-height": "90vh", "overflow-y" : "scroll"}}>
       <div className="list-group">
-        {countries.map((countrie) => {
+        {countries.map((country) => {
           return (
             <Link
               className="list-group-item list-group-item-action"
-              to={`/${countrie.alpha3Code}`}
+              to={`/${country.alpha3Code}`}
             >
               <img
-                src={`https://flagpedia.net/data/flags/icon/72x54/${countrie.alpha2Code.toLowerCase()}.png`}
-                alt={`Flag of ${countrie.name.common}`}
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt={`Flag of ${country.name.common}`}
               />
-              <p>{countrie.name.common}</p>
+              <p>{country.name.common}</p>
             </Link>
           );
         })}
