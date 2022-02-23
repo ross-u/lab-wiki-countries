22857b17bdfc4702ee6d5b07d8f2965f87fa989d
Gonzalo Otero <gonzalo.chalo@gmail.com>
2022-02-23 17:53:43 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import CountriesList from './components/CountriesList';
 
+import countriesList from './countries.json'
+
+import NavBar from './components/Navbar';
+
+import CountryDetails from './components/CountryDetails';
+
+import { Routes, Route } from "react-router-dom";
+
+import { useState, useEffect } from "react";
+
+function App() {
+  
+  const [countries, setCountries] = useState([]);
+ 
+  useEffect(() => {
+    setCountries(countriesList);
+  }, [countries]);
+
+  return <div className="App">
+
+  <NavBar></NavBar>
+ 
+ <div className="container">
+
+ <div className="row">
+
+  <Routes>
+        {/* <Route  path="/" element={ <HomePage /> } /> */}
+ 
+      
+        <Route path="/countries/:id" element={ <CountryDetails /> } />
+
+          <Route path="/" element={ <CountriesList countries={countries}/> } />
+
+
+        
+       
+ 
+      </Routes>
+      </div>
+ </div>
+  </div>
+}
 export default App;
+
+
