5640c59a3d1a4da19121c41d3d6538c221c9585f
angelita15 <95506994+angelita15@users.noreply.github.com>
2022-02-24 09:15:08 +0100

    [MADE] angela rueda

---
+++ src/Components/CountryDetails/CountryDetails.js
@@ -0,0 +1,15 @@
+
+import countries from '../../countries.json'
+import { Link, useParams, } from "react-router-dom";
+
+function CountryDetails() {
+    return (
+        <div>CountryDetails</div>
+    )
+}
+
+
+
+
+
+export default CountryDetails
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">wiki-countries</span>
+            </div>
+        </nav>
+    )
+    }  
+ export default Navbar
